f6e1f73e7888c388b4bc72d5b0b3fd2012094683
pedrin <pedronog30@gmail.com>
2026-06-01 12:15:43 -0300

    JS HERO

---
+++ script.js
@@ -0,0 +1,18 @@
+const texto = "O espaço\nnunca esteve tao perto...";
+const elemento = document.getElementById("titulo-hero");
+let i = 0;
+
+function digitar() {
+  if (i < texto.length) {
+    if (texto[i] === "\n") {
+      elemento.innerHTML += "<br>";
+    } else {
+      elemento.innerHTML += texto[i];
+    }
+    i++;
+    setTimeout(digitar, 60);
+  }
+}
+
+digitar();
+
